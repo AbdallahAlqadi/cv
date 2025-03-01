f86e10c2b6ad6a82d39f660904f14ee829e33605
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2025-03-01 10:39:11 +0300

    ADD

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
-
-    sections.forEach(section => {
-        section.classList.add("section-animation");
-    });
-});
